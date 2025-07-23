47f25b261acc1eb3c916102f098c855159a2b979
V1P3R-XMR <v1p3r@null.net>
2025-07-23 17:40:19 +0100

    Delete remove_webhook.py

--- remove_webhook.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-BOT_TOKEN = "7480001572:AAFFgR6MG_pSDcXEPn_2OxP43uM8nwsejs4"
-
-url = f"https://api.telegram.org/bot{BOT_TOKEN}/deleteWebhook"
-params = {"drop_pending_updates": True}
-
-resp = requests.get(url, params=params)
-print(resp.json())
